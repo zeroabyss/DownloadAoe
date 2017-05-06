4f423b313b0005e329bef1b45d5790b16b7a88ff
zero <422359126@qq.com>
2017-05-06 23:34:46 +0800

    second

---
+++ app/src/main/java/zero/downloadaoe/test.java
@@ -0,0 +1,26 @@
+package zero.downloadaoe;
+
+/**
+ * Created by Aiy on 2017/5/6.
+ */
+
+public class test {
+    private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
